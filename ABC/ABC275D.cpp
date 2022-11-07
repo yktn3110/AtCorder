5609fa9a3fbc7873c59ff4641e78a03bd71ff6b1
yktn3110 <yuki.3110f@gmail.com>
2022-11-07 22:36:29 +0900

    add ABC276

--- ABC/ABC275D.cpp
+++ ABC/ABC275D.cpp
@@ -1,12 +1,25 @@
-#include <bits/stdc++.h>
+/* #include <bits/stdc++.h> */
+#include <iostream>
+#include <unordered_map>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 
+unordered_map<long long, long long> memo;
+
+long long f(long long x) {
+    long long ret;
+    if (x == 0) return 1;
+    if (memo.count(x)) return memo[x];
+    ret = f(x / 2) + f(x / 3);
+    return memo[x] = ret;
+}
+
 int main(void) {
-    int s;
+    long long s;
 
     cin >> s;
 
-    cout << 3 << endl;
+    cout << f(s) << endl;
+
     return 0;
 }
