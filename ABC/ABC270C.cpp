ba831b1dbf075d96a37223113fb95f98f5557047
yktn3110 <yuki.3110f@gmail.com>
2022-12-01 08:32:44 +0900

    add abc270c

---
+++ ABC/ABC270C.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rep2(i, n) for (int i = 1; i <= (n); i++)
+#define rep3(x, n) for (auto x : n)
+#define ALL(obj) (obj).begin(), (obj).end()
+
+bool done = false;
+vector<bool> flag(300000);
+vector<int> path;
+vector<int> G[300000];
+long long cnt = 0;
+void dfs_find_to(int from, int to) {
+    if (!done) path.push_back(from);
+    flag[from] = true;
+    if (from == to) done = true;
+
+    for (int i = 0; i < G[from].size(); i++) {
+        if (flag[G[from][i]]) continue;
+        cnt++;
+
+        dfs_find_to(G[from][i], to);
+    }
+
+    if (!done) path.pop_back();
+    return;
+}
+
+int main(void) {
+    int n, x, y;
+    cin >> n >> x >> y;
+
+    rep(i, n - 1) {
+        int a, b;
+        cin >> a >> b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+    rep2(i, n) flag[i] = false;
+
+    vector<int> dist;
+    dfs_find_to(x, y);
+
+    cout << "debug " << cnt << "\n";
+    rep3(x, path) cout << x << endl;
+    return 0;
+}
+/* ==================================================== */
+#if 0
+long long cnt = 0;
+map<int, vector<int>> G;
+vector<int> dfs_find_to(int from, int to) {
+    if (!done) path.push_back(from);
+    flag[from] = true;
+    if (from == to) done = true;
+    if (G[from].size() != 0) {
+        rep3(v, G[from]) {
+            if (flag[v]) continue;
+            cnt++;
+            dfs_find_to(v, to);
+        }
+    }
+    if (!done) path.pop_back();
+    return path;
+}
+
+int main(void) {
+    int n, x, y;
+    cin >> n >> x >> y;
+
+    rep(i, n - 1) {
+        int a, b;
+        cin >> a >> b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+
+    rep2(i, n) flag[i] = false;
+
+    dfs_find_to(x, y);
+
+    cout << "debug " << cnt << "\n";
+    rep3(x, path) cout << x << " ";
+    return 0;
+}
+#endif
