bb1c7b3b56436a945d2c23353ee7b08d1558596a
yktn3110 <yuki.3110f@gmail.com>
2022-12-10 18:52:12 +0900

    delete unneeded code

--- ABC/ABC274D.cpp
+++ ABC/ABC274D.cpp
@@ -37,38 +37,3 @@ int main(void) {
     if (f(x, xa) && f(y, ya)) cout << "Yes" << endl;
     else cout << "No" << endl;
 }
-
-/*
-int main(void) {
-    int n, x, y;
-    int sumx = 0, sumy = 0;
-
-    cin >> n >> x >> y;
-
-    vector<int> a(n);
-    rep(i, n) {
-        cin >> a[i];
-        if (i % 2) sumy += a[i];
-        else sumx += a[i];
-    }
-
-    for (ll bit = 0; bit < ((ll)1 << n); bit++) {
-        int temp1 = sumx;
-        int temp2 = sumy;
-        rep(i, n) {
-            if (i == 0) continue;
-            if (bit & (1 << i)) {
-                if (i % 2) temp2 -= 2 * a[i];
-                else temp1 -= 2 * a[i];
-            }
-        }
-        if ((x == temp1) && (y == temp2)) {
-            cout << "Yes" << endl;
-            return 0;
-        }
-    }
-    cout << "No" << endl;
-
-    return 0;
-}
- */
