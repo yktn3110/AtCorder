c6cb36feaa1749d6fb8c7476352e0859ed757cd7
yktn3110 <yuki.3110f@gmail.com>
2022-12-14 10:55:44 +0900

    delete unneeded code

--- ABC/ABC272D.cpp
+++ ABC/ABC272D.cpp
@@ -21,25 +21,6 @@ int main(void) {
         for (int j = -n; j < n; j++)
             if (j * j == m - i * i) l.push_back(P(i, j));
 
-    /* if (m == 1) l.push_back(P(1, 0));
-    else {
-        rep2(i, n) for (int j = i; j <= n; j++) if (j * j == m - i * i)
-            l.push_back(P(i, j));
-    }
-    rep2(i, n) rep2(j, n) {
-        rep3(dl, l) {
-            P now = P(i, j);
-            P dp1 = P(i + dl.first, j + dl.second);
-            P dp2 = P(i - dl.first, j + dl.second);
-            P dp3 = P(i + dl.second, j + dl.first);
-            P dp4 = P(i - dl.second, j + dl.first);
-            if (lim.count(dp1)) G[now].insert(dp1), G[dp1].insert(now);
-            if (lim.count(dp2)) G[now].insert(dp2), G[dp2].insert(now);
-            if (lim.count(dp3)) G[now].insert(dp3), G[dp3].insert(now);
-            if (lim.count(dp4)) G[now].insert(dp4), G[dp4].insert(now);
-        }
-    } */
-
     queue<P> que;
     vector dist(n + 1, vector<int>(n + 1, -1));
     dist[1][1] = 0;
@@ -55,11 +36,6 @@ int main(void) {
             dist[ni][nj] = dist[now.first][now.second] + 1;
             que.push(P(ni, nj));
         }
-        /* rep3(dp, G[now]) {
-            if (dist[dp.first][dp.second] != -1) continue;
-            dist[dp.first][dp.second] = dist[now.first][now.second] + 1;
-            que.push(dp);
-        } */
     }
 
     rep2(i, n) {
