6655cd4ac96ce8ce23c922e81305aa32b773f782
yktn3110 <yuki.3110f@gmail.com>
2025-03-23 08:22:19 +0900

    modify solution for ABC398C

--- ABC/abc398c.cpp
+++ ABC/abc398c.cpp
@@ -10,31 +10,26 @@ const long long INF = 1LL << 60;
 int main(void)
 {
     cin.tie(0)->sync_with_stdio(0);
-    int n;
-    cin >> n;
-    map<int,int> an;
-    map<int,int> mp;
-    vector<bool> flag(n+1,true);
-    flag[0] = false; 
-    
-    rep2(i,n) {
-        int a;
-        cin >> a;
-        an[i]= a;
+    int n,tr,tc;
+
+    cin >> n >> tr >> tc;
+
+    string s;
+    cin >> s;
+
+    set<pair<int,int>> smokes;
+    int r=0,c=0;
+    smokes.insert({r,c});
+    rep(i,n) {
+        char wind = s[i];
+        if (wind == 'N') r++;
+        else if (wind == 'W') c++;
+        else if (wind == 'S') r--;
+        else if (wind == 'E') c--;
+        smokes.insert({r,c});
+        if (smokes.count({r+tr,c+tc})) cout << 1;
+        else cout << 0; 
     }
-    rep2(i,n) {
-        if (mp.find(an[i]) != mp.end()) {
-            flag[i] = false;
-            flag[mp[an[i]]] = false;
-        } else {
-            mp[an[i]] = i;
-        }
-    }
-    int max=-1;
-    rep2(i,n) {
-        if (!flag[i]) continue; 
-        if (an[i] > an[max]) max =i;
-    }
-    cout << max << endl;
+    cout << endl;
     return 0;
 }
